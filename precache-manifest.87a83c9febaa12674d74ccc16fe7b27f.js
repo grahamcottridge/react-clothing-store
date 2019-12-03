8c99ac5b66722d5734fa041345a85e33618f56ad
Graham Cottridge <codinggc@gmail.com>
2019-12-03 02:39:50 +0000

    Updates

---
+++ precache-manifest.87a83c9febaa12674d74ccc16fe7b27f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6562ae03af394c191705efb08a1f8c60",
+    "url": "/react-clothing-store/index.html"
+  },
+  {
+    "revision": "6d5e5207c663dbc6f97c",
+    "url": "/react-clothing-store/static/css/main.0ed211bd.chunk.css"
+  },
+  {
+    "revision": "3fa1d3399bbb6b813f30",
+    "url": "/react-clothing-store/static/js/2.33989df0.chunk.js"
+  },
+  {
+    "revision": "6d5e5207c663dbc6f97c",
+    "url": "/react-clothing-store/static/js/main.4d9f6d2d.chunk.js"
+  },
+  {
+    "revision": "b9cfd962ba1d9008edb1",
+    "url": "/react-clothing-store/static/js/runtime~main.2ce16248.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/react-clothing-store/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/react-clothing-store/static/media/shopping-bag.30f07071.svg"
+  }
+]);
